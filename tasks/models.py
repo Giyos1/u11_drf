8d177c0a24ac9a2da5096385f5e5038c61c6f9c7
giyos <giyos@giyoss-MacBook-Pro.local>
2026-03-31 20:15:05 +0500

    add change

--- tasks/models.py
+++ tasks/models.py
@@ -43,6 +43,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
     status = models.CharField(max_length=255, choices=Status.choices, default=Status.TO_DO)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
